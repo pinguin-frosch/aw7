f7f494db4bfccdf08c52a083ec3951e015f1ba87
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-07-06 22:59:09 -0400

    Funciones de sobrecarga de CPU

--- app/aws7/urls.py
+++ app/aws7/urls.py
@@ -6,5 +6,6 @@ from aws7 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('sobrecarga/', views.sobrecarga, name='sobrecarga'),
     path('tareas/', include('tareas.urls'))
 ]
--- app/aws7/views.py
+++ app/aws7/views.py
@@ -1,6 +1,15 @@
+import math
+
 from django.http import HttpRequest
 from django.shortcuts import render
 
 
 def index(request: HttpRequest):
     return render(request, 'index.html')
+
+
+def sobrecarga(request: HttpRequest):
+    sum = 0
+    for i in range(10**7):
+        sum += math.sqrt(i) * math.sin(i) * math.cbrt(i)
+    return render(request, 'sobrecarga.html')
